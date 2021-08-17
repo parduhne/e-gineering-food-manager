aad444133841f8408be95cce0a95fa258f73b43d
Evan Parduhn <parduhne@Evans-MacBook-Pro.local>
2021-08-17 13:41:21 -0400

    added keysj

--- e-gineering-react-training/src/App.tsx
+++ e-gineering-react-training/src/App.tsx
@@ -3,23 +3,38 @@ import { forEachChild, isTemplateSpan } from "typescript";
 type Food = {
   name: string;
   quantity: number;
-}
+  reorderPoint: number;
+  type: string;
+};
 
 const foods: Food[] = [
-  {name: "Carrot", quantity: 5},
-  {name: "Pumpkin", quantity: 6}
+  { name: "Carrot", quantity: 5, reorderPoint: 1, type: "veggie" },
+  { name: "Pumpkin", quantity: 6, reorderPoint: 0, type: "veggie" },
 ];
 
 function App() {
-  function renderFoods() {
-    return foods.map((food: Food) => <li>{food.name} qty: {food.quantity}</li>)
-  }
   return (
     <>
       <h1>Pantry Manager</h1>
-      <ul>
-        {renderFoods()}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Qty</th>
+            <th>Min Qty</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        {foods.map((food: Food) => (
+          <tr key={food.name}>
+            {/* {Object.values(foods).map((item) => <td>{item}</td> )} */}
+            <td>{food.name}</td>
+            <td>{food.quantity}</td>
+            <td>{food.reorderPoint}</td>
+            <td>{food.type}</td>
+          </tr>
+        ))}
+      </table>
     </>
   );
 }
